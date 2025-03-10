587fb854897b5f17769432997842df18b6a12db0
namxle <103374580+namxle@users.noreply.github.com>
2025-03-10 12:10:38 +0700

    Revert "feat: run pri 01 in parallel"
    This reverts commit 92ed76b47837a27343c2d5fc9ff5d5152a42f6ae.

--- filtration_prioritization_hg19/SCIP_pri_01_hg19.pl
+++ filtration_prioritization_hg19/SCIP_pri_01_hg19.pl
@@ -2,8 +2,6 @@
 use strict;
 use Getopt::Std;
 use Cwd 'abs_path';
-use Parallel::ForkManager;
-
 my %opts;
 getopt ('n:u:@:',\%opts);
 my $name=$opts{"n"};
@@ -38,16 +36,10 @@ while (<file1>){
 close file1;
 close out1;
 
-my $pm = Parallel::ForkManager->new($threads);
-
 open file1, "<$dir/$name.$num.variant_list.txt";
 while (<file1>){
  chomp;
  my @split1=split /\t/,$_;
-
- # Fork a child process for parallel execution
- $pm->start and next;
-
  my $start=localtime();
  my $start_time=time();
  print "$start\t$_\n";
@@ -85,9 +77,7 @@ while (<file1>){
   close out1;
   system ("gzip -f $dir/app_temp_file/$split1[0]/$split1[0].$split1[1].$split1[2].$split1[3].$split1[4].log.txt");
  }
- $pm->finish;  # Child process exits
 }
-$pm->wait_all_children;  # Wait for all processes to finish
 close file1;
 
 system ("rm $dir/$name.$num.variant_list.txt");
