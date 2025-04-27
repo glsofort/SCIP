dd8e1ed642dd1d7c6d798a906513a8d1edfab152
namxle <103374580+namxle@users.noreply.github.com>
2025-04-27 21:45:52 +0700

    fix: remove parallel on samtools command to avoid errors

--- filtration_prioritization_hg38_bak/SCIP_pri_01_hg38.pl
+++ filtration_prioritization_hg38_bak/SCIP_pri_01_hg38.pl
@@ -55,8 +55,8 @@ while (<file1>){
  # comment out this bracket if want to re-generate temporary files for all existing variants
  unless (-e "$dir/app_temp_file/$split1[0]/$split1[0].$split1[1].$split1[2].$split1[3].$split1[4].log.txt.gz"){
   system ("mkdir -p $dir/app_temp_file/$split1[0]"); 
-  system ("perl $dir/SCIP_pri_03_hg38.pl -c $split1[1] -s $split1[2] -e $split1[3] -p $split1[0] -t $split1[4] -@ 4");
-  system ("perl $dir/SCIP_pri_04_hg38.pl -c $split1[1] -s $split1[2] -e $split1[3] -p $split1[0] -t $split1[4] -@ 4");
+  system ("perl $dir/SCIP_pri_03_hg38.pl -c $split1[1] -s $split1[2] -e $split1[3] -p $split1[0] -t $split1[4] -@ 1");
+  system ("perl $dir/SCIP_pri_04_hg38.pl -c $split1[1] -s $split1[2] -e $split1[3] -p $split1[0] -t $split1[4] -@ 1");
   system ("perl $dir/SCIP_pri_05_hg38.pl -c $split1[1] -s $split1[2] -e $split1[3] -p $split1[0] -t $split1[4]");
   system ("perl $dir/SCIP_pri_06_hg38.pl -c $split1[1] -s $split1[2] -e $split1[3] -p $split1[0] -t $split1[4]");
   system ("perl $dir/SCIP_pri_07_hg38.pl -c $split1[1] -s $split1[2] -e $split1[3] -p $split1[0] -t $split1[4]");
